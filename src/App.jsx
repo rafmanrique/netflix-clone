35a2d2a1b2c84590118e25b073aabe0dc3db736e
Rafael Manrique <rafamanrique26@gmail.com>
2022-07-13 12:23:52 -0400

    Login page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home.jsx";
 import "./app.scss"
 import Watch from "./pages/home/watch/Watch.jsx";
 import Register from "./pages/register/Register.jsx";
+import Login from "./pages/login/Login.jsx";
 
 const App = () => {
   return <Register/>
