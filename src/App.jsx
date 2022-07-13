28deaa7f770a3804361c3bfb5da2a64302b6f608
Rafael Manrique <rafamanrique26@gmail.com>
2022-07-13 11:38:14 -0400

    Register page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home.jsx";
 import "./app.scss"
 import Watch from "./pages/home/watch/Watch.jsx";
+import Register from "./pages/register/Register.jsx";
 
 const App = () => {
-  return <Watch/>;
+  return <Register/>
 };
 
 export default App;
